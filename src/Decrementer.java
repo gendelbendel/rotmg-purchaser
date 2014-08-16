94a51ee8b71c2bdecfde702866876ba2edc6c8ec
Dave <Dave@Gendelbendel>
2014-08-15 20:36:34 -0700

    Initial commit

---
+++ src/Decrementer.java
@@ -0,0 +1,34 @@
+import java.util.concurrent.CountDownLatch;
+
+/* Decrementer.java
+ * 
+ * Decrementer is a simple class that uses a CountDownLatch and counts it down in one second intervals.
+ * 
+ * @author dwdude
+ * 
+ * 
+ */
+
+public class Decrementer implements Runnable {
+	
+	CountDownLatch latch = null;
+
+    public Decrementer(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void run() {
+
+        try {
+        	while(latch.getCount() != 0){
+        		System.out.println("Count: " + latch.getCount());
+        		Thread.sleep(1000);
+                this.latch.countDown();
+        	}
+        	System.out.println("STARTED THREADS!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/MultiplePackagePurchaser.java
@@ -0,0 +1,66 @@
+import java.util.concurrent.CountDownLatch;
+
+
+/* MultiplePackagePurchaser.java
+ * 
+ * MultiplePackagePurchaser is used for sending multiple requests at the same time
+ * to purchase a package multiple times.
+ * 
+ * This was written to attempt to receieve multiple free backpacks during the promo, where
+ * it was successful in doing so.
+ * 
+ * @author dwdude
+ * 
+ * 
+ */
+
+public class MultiplePackagePurchaser 
+{
+
+	/*
+	 * ONLY CHANGE INFORMATION BELOW THIS COMMENT BLOCK
+	 * 
+	 */
+	
+	// The email for your RotMG account
+	public static String yourEmail = "test@gmail.com";
+	
+	// The password for your RotMG account
+	public static String yourPassword = "testpassword";
+	
+	// The package id of the package you'd like to purchase
+	public static int packageId = 377;
+	
+	// The amount of times you'd like to attempt to purchase the package.
+	// Anything above 100 doesn't typically work well.
+	public static int timesToRun = 100; 
+	
+	/*
+	 * ONLY CHANGE INFORMATION ABOVE THIS COMMENT BLOCK
+	 * 
+	 */
+	
+	public static void main(String[] args)
+	{
+		try {
+			manyPurchases(timesToRun);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void manyPurchases(int amt) throws InterruptedException
+	{
+		CountDownLatch myLatch = new CountDownLatch(3);
+		
+		Decrementer dec = new Decrementer(myLatch);
+		
+		for(int i = 0; i < amt; i++)
+		{
+			new Thread(new Purchase(myLatch, i, yourEmail, yourPassword, packageId)).start();
+		}
+		System.out.println("Starting countdown.");
+		new Thread(dec).start();
+				
+	}
+}
